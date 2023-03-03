dd515725f297eea5967feb378dee32875312c06f
Stas Yakobov <stas@3os.org>
2023-03-03 17:19:06 +0200

    20230303.1719

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -O https://raw.githubusercontent.com/fire1ce/raspberry-pi-pir-motion-display-control/main/motion-display-control.py
-curl -O https://raw.githubusercontent.com/fire1ce/raspberry-pi-pir-motion-display-control/main/motion-display-control.service
+curl -O https://raw.githubusercontent.com/fire1ce/raspberry-pi-ld2410b-motion-display-control/main/motion-display-control.py
+curl -O https://raw.githubusercontent.com/fire1ce/raspberry-pi-ld2410b-motion-display-control/main/motion-display-control.service
 
 if [ ! -d "/usr/local/bin" ]; then
   sudo mkdir -p /usr/local/bin
